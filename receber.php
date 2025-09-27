e59e3d621a737242af8d3a7cc90bcc64896773df
Maria Eduarda Gomes <165100963+eduardaag0mess@users.noreply.github.com>
2025-09-26 22:12:06 -0300

    Add files via upload

---
+++ receber.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+<form action="receber.php" method="POST">
+      <h2 Calculadora </h2>
+<label for="nome">Digite o Primeiro Numero: </label><br>
+   <input type="text" id="numero1" name="numero1" required> <br>
+   <label for="email">Digite o Segundo Numero</label><br>
+   <input type="text" id="numero2" name="numero2" required><br><
+        <label for="operacoes">Escolha a Operação</label><br>
+          <select id="op" name="op">
+          <option value="+">SOMA</option>
+          <option value="-">SUBTRAIR</optio>
+          <option value="*">MULTIPLICAR</option>
+          <option value="/">DIVIDIR</option>
+               </select><br><br>
+               <button type="submit">Calcular</button>
+
+</form>
+<form action="receber.php" method="POST">
+<h2 Calculadora </h2>
+<label for="nome">Digite o Primeiro Numero: </label><br>
+<input type="text" id="numero1" name="numero1" required> <br>
+<label for="email">Digite o Segundo Numero</label><br>
+<input type="text" id="numero2" name="numero2" required><br><
+<label for="operacoes">Escolha a Operação</label><br>
+<select id="op" name="op">
+<option value="+">SOMA</option>
+<option value="-">SUBTRAIR</option>
+<option value="*">MULTIPLICAR</option>
+<option value="/">DIVIDIR</option>
+</select><br><br>
+<button type="submit">Calcular</button>
+</form>
